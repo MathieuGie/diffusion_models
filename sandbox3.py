edf10bf3ef4659e08b8eb5ce6a0cf6619c0c0074
Mathieu Gierski <mathieugierski@mbp-de-mathieu.home>
2024-01-06 19:13:15 +0100

    diffusion pipeline to improve

--- sandbox3.py
+++ sandbox3.py
@@ -19,18 +19,15 @@ class CNN(pl.LightningModule):
 
         self.relu = nn.ReLU()
         # Encoder layers
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=256, kernel_size=25, padding=1)
-        self.maxpolling1 = nn.MaxPool2d(padding=1, kernel_size=4)
-        self.conv2 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=5, padding=1)
-        self.maxpolling2 = nn.MaxPool2d(padding=1, kernel_size=5)
-        self.conv3 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=6, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=256, kernel_size=20, padding=1, stride=2)
+        self.maxpolling1 = nn.MaxPool2d(padding=1, kernel_size=2)
+        self.conv2 = nn.Conv2d(in_channels=256, out_channels=1024, kernel_size=4, padding=1)
+        self.maxpolling2 = nn.MaxPool2d(padding=1, kernel_size=2)
+        self.conv3 = nn.Conv2d(in_channels=1024, out_channels=4096, kernel_size=5)
 
         # Decoder layers
-        self.deconv1 = nn.ConvTranspose2d(in_channels=1025, out_channels=256, kernel_size=4, padding=1)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=256, out_channels=64, kernel_size=4, stride=2, padding=1)  
-        self.deconv3 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1) 
-        self.deconv4 = nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=6, stride=3, padding=1)   
-        self.deconv5 = nn.ConvTranspose2d(in_channels=16, out_channels=3, kernel_size=6, stride=4, padding=1)      
+        self.linear1 = nn.Linear(4097, 12288)
+        self.linear2 = nn.Linear(12288, 7500)
 
     def forward(self, x, t):
         # Encoding
@@ -51,17 +48,14 @@ class CNN(pl.LightningModule):
         x = torch.cat((x, t), dim=1)
         #print(x.shape)
 
+        x = x.view(x.size(0), -1)
+        #print(x.shape)
         # Decoding
-        x = self.relu(self.deconv1(x))
+        x = self.relu(self.linear1(x))
+        x = self.linear2(x)
         #print(x.shape)
-        x = self.relu(self.deconv2(x))
-        #print(x.shape)
-        x = self.relu(self.deconv3(x))
-        #print(x.shape)
-        x = self.relu(self.deconv4(x))
-        #print(x.shape)
-        x = self.relu(self.deconv5(x))
-        #print(x.shape)
+
+        x = x.view(x.size(0), 3, 50, 50)
 
         return x
     
@@ -70,7 +64,7 @@ class CNN(pl.LightningModule):
         # Basic function to compute the loss
         noisy_imgs, clean_imgs, t = batch
         outputs = self(noisy_imgs, t)
-        loss = torch.nn.functional.mse_loss(outputs, clean_imgs)
+        loss = torch.nn.functional.mse_loss(outputs, noisy_imgs-clean_imgs)
         return loss
     
 
@@ -96,7 +90,7 @@ class CNN(pl.LightningModule):
 
 
 class DenoisingDataset(Dataset):
-    def __init__(self, base_dir, transform=None, file_list=None, subset_fraction=0.15):
+    def __init__(self, base_dir, transform=None, file_list=None, subset_fraction=0.8):
         self.base_dir = base_dir
         self.transform = transform
 
@@ -106,13 +100,13 @@ class DenoisingDataset(Dataset):
         # If file_list is None, use all images in the directory
         if file_list is None:
             file_list = []
-            for step in range(1, 11):
+            for step in range(1, 13):
                 step_dir = os.path.join(base_dir, f'step_{step}')
                 if os.path.exists(step_dir):
                     file_list.extend(os.listdir(step_dir))
 
         # Iterate over all noising steps except the first one (step_0)
-        for step in range(1, 11):
+        for step in range(1, 13):
             current_step_dir = os.path.join(base_dir, f'step_{step}')
             previous_step_dir = os.path.join(base_dir, f'step_{step-1}')
 
@@ -125,7 +119,7 @@ class DenoisingDataset(Dataset):
 
                         # Check if the corresponding less noisy image exists in the previous step
                         if os.path.exists(previous_img_path):
-                            self.image_pairs.append((current_img_path, previous_img_path, step/10))
+                            self.image_pairs.append((current_img_path, previous_img_path, step/12))
 
         self.image_pairs = random.sample(self.image_pairs, int(len(self.image_pairs) * subset_fraction))
 
@@ -160,13 +154,13 @@ dir_all =  '/Users/mathieugierski/Library/CloudStorage/OneDrive-Personnel/Diffus
 image_files = os.listdir(dir_images)
 random.shuffle(image_files)
 
-train_files, test_files = train_test_split(image_files, test_size=0.2)  # Adjust test_size as needed
+train_files, test_files = train_test_split(image_files, test_size=0.3)  # Adjust test_size as needed
 #print(len(train_files))
 train_dataset = DenoisingDataset(dir_all, transform, file_list=train_files)
 test_dataset = DenoisingDataset(dir_all, transform, file_list=test_files)
 
-dataloader_train = DataLoader(train_dataset, batch_size=32, shuffle=True)
-dataloader_test = DataLoader(test_dataset, batch_size=32, shuffle=True)
+dataloader_train = DataLoader(train_dataset, batch_size=128, shuffle=True)
+dataloader_test = DataLoader(test_dataset, batch_size=128, shuffle=True)
 
 #Devise:
 if not torch.backends.mps.is_available():
@@ -185,7 +179,7 @@ model.to(mps_device)
 
 mlf_logger = MLFlowLogger(experiment_name="lightning_logs", tracking_uri="file:./ml-runs")
 
-trainer = pl.Trainer(max_epochs=10, accelerator="mps", logger=mlf_logger, log_every_n_steps=1, val_check_interval=0.5)
+trainer = pl.Trainer(max_epochs=100, accelerator="mps", logger=mlf_logger, log_every_n_steps=1, val_check_interval=0.5)
 trainer.fit(model, dataloader_train, dataloader_test)
 
 
@@ -194,18 +188,18 @@ trainer.fit(model, dataloader_train, dataloader_test)
 folder_name = "generated_images"
 os.makedirs(folder_name, exist_ok=True)
 
-image = torch.rand((3,100,100))
+image = torch.rand((3,50,50))
 step=1
 
-for i in range(10):
+for i in range(12):
 
     batched_image = image.unsqueeze(0)
 
     step_tensor = torch.zeros((1,1,1,1))
     step_tensor[0,0,0,0]=step
 
-    image = model(batched_image, step_tensor)[0,:,:,:]
-    step-=1/10
+    image -= model(batched_image, step_tensor)[0,:,:,:]
+    step-=1/12
 
     pil_image = TF.to_pil_image(image)
 
