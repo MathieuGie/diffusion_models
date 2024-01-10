dbc4922774cdbe9886a4f437999d1012802ef8db
Mathieu Gierski <mathieugierski@nat-eduroam-138-gw-01-sif.saclay.inria.fr>
2024-01-10 16:33:11 +0100

    clearer hyperparam tuning

--- encdec.py
+++ encdec.py
@@ -15,6 +15,14 @@ import torchvision.transforms.functional as TF
 batches = 128
 epochs = 800
 
+a=8
+b=16
+c=24
+d=32
+e=40
+f=48#
+g=56#
+
 
 class Encoder(pl.LightningModule):
     def __init__(self):
@@ -24,26 +32,25 @@ class Encoder(pl.LightningModule):
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=4, padding=1, stride=2)
-        self.norm1 = nn.BatchNorm2d(16)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=a, kernel_size=4, padding=1, stride=2)
+        self.norm1 = nn.BatchNorm2d(a)
 
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, padding=1, stride=2)
-        self.norm2 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(in_channels=a, out_channels=b, kernel_size=4, padding=1, stride=2)
+        self.norm2 = nn.BatchNorm2d(b)
 
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, padding=1, stride=2)
-        self.norm3 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(in_channels=b, out_channels=c, kernel_size=4, padding=1, stride=2)
+        self.norm3 = nn.BatchNorm2d(c)
 
-        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, stride=2)
-        self.norm4 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(in_channels=c, out_channels=d, kernel_size=3, padding=1, stride=2)
+        self.norm4 = nn.BatchNorm2d(d)
 
-        self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=2)
-        self.norm5 = nn.BatchNorm2d(256)
+        self.conv5 = nn.Conv2d(in_channels=d, out_channels=e, kernel_size=3, padding=1, stride=2)
+        self.norm5 = nn.BatchNorm2d(e)
 
-        self.conv6 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=2)
-        self.norm6 = nn.BatchNorm2d(512)
+        self.conv6 = nn.Conv2d(in_channels=e, out_channels=f, kernel_size=3, padding=1, stride=2)
+        self.norm6 = nn.BatchNorm2d(f)
 
-        self.conv7 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=4)
-        self.norm7 = nn.BatchNorm2d(1024)
+        self.conv7 = nn.Conv2d(in_channels=f, out_channels=g, kernel_size=4)
 
   
     def forward(self, x):
@@ -58,11 +65,11 @@ class Encoder(pl.LightningModule):
         #print(x.shape)
         x = self.relu(self.norm5(self.conv5(x)))#7
         #print(x.shape)
-        x = self.relu(self.norm6(self.conv6(x)))#4
+        #x = self.relu(self.norm6(self.conv6(x)))#4
 
-        x = self.relu(self.norm7(self.conv7(x)))
+        #x = self.relu(self.conv7(x))
 
-        #print("end of encoder", x.shape)
+        print("end of encoder", x.shape)
 
         return x
     
@@ -73,32 +80,32 @@ class Decoder(pl.LightningModule):
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
-        self.deconv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=4)
-        self.norm1 = nn.BatchNorm2d(512)
+        self.deconv1 = nn.ConvTranspose2d(in_channels=g, out_channels=f, kernel_size=4)
+        self.norm1 = nn.BatchNorm2d(f)
 
-        self.deconv2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, padding=1, stride=2)
-        self.norm2 = nn.BatchNorm2d(256)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=f, out_channels=e, kernel_size=3, padding=1, stride=2)
+        self.norm2 = nn.BatchNorm2d(e)
 
-        self.deconv3 = nn.ConvTranspose2d(in_channels=256, out_channels=124, kernel_size=3, padding=1, stride=2)
-        self.norm3 = nn.BatchNorm2d(124)
+        self.deconv3 = nn.ConvTranspose2d(in_channels=e, out_channels=d, kernel_size=3, padding=1, stride=2)
+        self.norm3 = nn.BatchNorm2d(d)
 
-        self.deconv4 = nn.ConvTranspose2d(in_channels=124, out_channels=64, kernel_size=3, padding=1, stride=2)
-        self.norm4 = nn.BatchNorm2d(64)
+        self.deconv4 = nn.ConvTranspose2d(in_channels=d, out_channels=c, kernel_size=3, padding=1, stride=2)
+        self.norm4 = nn.BatchNorm2d(c)
 
-        self.deconv5 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, padding=1, stride=2)
-        self.norm5 = nn.BatchNorm2d(32)
+        self.deconv5 = nn.ConvTranspose2d(in_channels=c, out_channels=b, kernel_size=4, padding=1, stride=2)
+        self.norm5 = nn.BatchNorm2d(b)
 
-        self.deconv6 = nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=4, padding=1, stride=2)
-        self.norm6 = nn.BatchNorm2d(16)
+        self.deconv6 = nn.ConvTranspose2d(in_channels=b, out_channels=a, kernel_size=4, padding=1, stride=2)
+        self.norm6 = nn.BatchNorm2d(a)
 
-        self.deconv7 = nn.ConvTranspose2d(in_channels=16, out_channels=3, kernel_size=4, padding=1, stride=2)
+        self.deconv7 = nn.ConvTranspose2d(in_channels=a, out_channels=3, kernel_size=4, padding=1, stride=2)
 
 
     def forward(self, x):
 
-        x = self.relu(self.norm1(self.deconv1(x)))
+        #x = self.relu(self.norm1(self.deconv1(x)))
         #print(x.shape)
-        x = self.relu(self.norm2(self.deconv2(x))) 
+        #x = self.relu(self.norm2(self.deconv2(x))) 
         #print(x.shape)
         x = self.relu(self.norm3(self.deconv3(x))) 
         #print(x.shape)
